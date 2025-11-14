37920387666cef0f0eb1381f10f47db3abcd5de7
devanshi-abs <devanshi.abstractit@gmail.com>
2025-11-14 15:00:31 +0530

    Removed unused code

--- Classes/AppDelegate.h
+++ Classes/AppDelegate.h
@@ -26,55 +26,6 @@
 #define  _APP_DELEGATE_H_
 
 #include "cocos2d.h"
-//#include "Header.h"
-
-extern int TotalCoin;
-extern int whichLevelSelected;
-extern int cntExitPopYesTap,cntExitIcon,cntExitLayout,cntMoreIconAds;
-extern int WhichScene,whichSubLvl,totalSubLvl[10];
-extern bool isExitShow,isLoadingScreen,isSkipLvl,isLvlCmp[10],isBackBtn,isLevelCompete,isFirstTimeAds,isCommonPopupOpen,isLevelScreen;
-extern float viewScaleY;
-extern cocos2d::Size ResolutionSize;
-extern float Device_Resosultion_Height;
-extern std::string strVocalLang;
-extern std::string AndroidJniPackage;
-extern int LanguageID;
-
-extern int whichMinigame;
-extern bool isMinigameLS,isColorsLS;
-extern bool isRewardBalloon,isPlayArea;
-extern int WhichFillClrPg,WhichClrPg;
-
-extern float XSpaceToPadding;
-extern float YSpaceToPadding;
-extern bool isAppreciationPopup,isNativeAds,isCollapsiveAds;
-extern int whichPopToySelected;
-extern bool isCmpPopToy[30];
-extern bool isUnLockPopToy[30];
-extern bool isRRVehicleUnLock[20];
-extern int TTSImageNo;
-extern bool isNormalColor;
-//=====Screen Time=====//
-extern time_t _time1,_time2;
-extern struct std::tm _startTime;
-extern struct std::tm _endTime;
-extern std::string PhotoSaveName;
-
-extern std::map<int,std::string> str_Level_AssetPack;
-extern std::vector<std::string> str_DownloadedAssetPacksList;
-
-extern bool isLevelAssestDownloaded[50];
-extern bool isCoinPanelShown;
-extern int DressUpUnLock[20][50];
-
-//extern int CDToyNo;
-
-extern int ambuAirToolCoin[20],ambuCleanToolCoin[20],bulldozerSerToolCoin[20],busAirToolCoin[20],craneDecorToolCoin[10][20],craneCleanToolCoin[20],jcbAirToolCoin[20],levelLockCoin[100],policeDecorToolCoin[10][20],policeSerToolCoin[20],tractorAirToolCoin[20],tractorCleanToolCoin[20],tractorDecorToolCoin[10][20];
-
-extern int cntStickers;
-
-extern bool isLevelLock[20],isMinigameLock[20];
-
 
 /**
  @brief    The cocos2d Application.
@@ -108,25 +59,7 @@ public:
      */
     virtual void applicationWillEnterForeground();
     
-    static void GetBannerHeight(cocos2d::Layer *scene_name);
-    
-    //    static void ShowMore();
-    //    static void ShowInterstitial();
-    //    static void ShowRewardVideo();
-    //
-    //    static void ShowNativeAds();
-    //    static void RemoveNativeAds();
-    
-    static bool getBackTouchValue();
-    
-    static std::string getTranslation(const std::string& key, int languageIndex);
-    static std::string getTranslation(const std::string& key);
-    
-    
-#pragma mark Screen Timing
-    static void storeUserPlayTime();
-    static void sendScreenName(std::string screenName);
-    static void sendLevelCompleteStatus(std::string LevelStatus);//LevelStatus -> Complete or ExitHalfWay
+  
 };
 
 #endif // _APP_DELEGATE_H_
